d1af686f5b91baa14854513a0b49fa794a8b93f4
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 15:50:09 +0200

    feat(apiClient): add signIn and logout functions for authentication

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,3 +1,5 @@
+import { SignInFormData } from "./pages/SignIn";
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const validateToken = async () => {
@@ -10,4 +12,33 @@ export const validateToken = async () => {
     throw new Error("Invalid Token");
   }
   return response.json();
+};
+
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/admin_auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const body = await response.json();
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+
+  return body;
+};
+
+export const logout = async () => {
+  const res = await fetch(`${API_BASE_URL}/api/admin_auth/logout`, {
+    credentials: "include",
+    method: "POST",
+  });
+
+  if (!res.ok) {
+    throw new Error("Error during sign out");
+  }
 };
