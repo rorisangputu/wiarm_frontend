61dc183d97ed39b363cfb717282460ea502d203b
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 10:55:33 +0200

    feat(App): implement routing with Layout and Home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1>Ola</h1>
-    </>
+    <div>
+      <Routes>
+        {/* Home */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
