cf0d54652bd740a3ae9ccfe50e848fb0f7809c4c
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 11:34:43 +0200

    feat(App): reorganize imports and add routes for About Us, Campaigns, and Contact pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import Campaigns from "./pages/Campaigns";
+import CampaignDetail from "./components/Campaigns/CampaignDetails";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -12,6 +16,40 @@ function App() {
           element={
             <Layout>
               <Home />
+            </Layout>
+          }
+        />
+        {/* About Us */}
+        <Route
+          path="/about-us"
+          element={
+            <Layout>
+              <AboutUs />
+            </Layout>
+          }
+        />
+        {/* Campaigns */}
+        <Route
+          path="/campaigns"
+          element={
+            <Layout>
+              <Campaigns />
+            </Layout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Layout>
+              <Contact />
+            </Layout>
+          }
+        />
+        <Route
+          path="/campaigns/:id"
+          element={
+            <Layout>
+              <CampaignDetail />
             </Layout>
           }
         />
