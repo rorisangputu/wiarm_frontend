bbe1b1849673754de03f5bfdce6f0cf0ee129d62
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 20:38:48 +0200

    feat(NewCampaign): create NewCampaign component for campaign creation

---
+++ src/pages/NewCampaign.tsx
@@ -0,0 +1,10 @@
+const NewCampaign = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Create New Campaign</h1>
+      {/* Add form for creating a campaign */}
+    </div>
+  );
+};
+
+export default NewCampaign;
