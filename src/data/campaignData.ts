a219e36b7b20802951477ca7c2e71f9ddede656f
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 11:28:26 +0200

    feat(Campaigns): add campaign data with details for three initiatives

---
+++ src/data/campaignData.ts
@@ -0,0 +1,33 @@
+import women from "../assets/two_women_farming.jpg";
+import mulch from "../assets/mulch_farming.webp";
+import sov from "../assets/sov_food.jpg";
+
+export const campaigns = [
+  {
+    id: 1,
+    title: "Seeds of Resistance",
+    location: "Eastern Cape, RSA",
+    date: "March 20, 2025",
+    description:
+      "This campaign brings together women farmers to reclaim ancestral land...",
+    picture: women,
+  },
+  {
+    id: 2,
+    title: "Harvesting Dignity Mission",
+    location: "KwaZulu-Natal",
+    date: "May 10, 2025",
+    description:
+      "Focused on building food sovereignty, this project supports women-led cooperatives...",
+    picture: sov,
+  },
+  {
+    id: 3,
+    title: "Agri-Education for Liberation",
+    location: "Venda, Limpopo",
+    date: "March 20, 2025",
+    description:
+      "This campaign empowers women through education, soil healing workshops...",
+    picture: mulch,
+  },
+];
