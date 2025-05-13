dcdec99bb4045e1b4f143c9543149c2faaf79788
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 10:55:56 +0200

    feat(Layout): update Layout component to include Header and Footer with min-height styling

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,16 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col">
+      <Header />
       <div>{children}</div>
+      <Footer />
     </div>
   );
 };
