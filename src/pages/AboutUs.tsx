ee7413ba8f311b127f1f99c3ada1793e16c8f63a
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 11:34:54 +0200

    feat(AboutUs): create AboutUs page with components for Hero, About, Team, and Action

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,19 @@
+import Hero from "../components/About/Hero";
+import About from "../components/About/About";
+import Team from "../components/About/Team";
+import Action from "../components/Action";
+import MissionValuesAbout from "../components/About/MissionValue";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <MissionValuesAbout />
+      <Team />
+      <Action />
+    </div>
+  );
+};
+
+export default AboutUs;
