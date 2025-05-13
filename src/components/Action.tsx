48fdc99426e40f1b9305247559f9235d4fdbc786
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 11:08:15 +0200

    feat(Action): implement Action component with background image and call-to-action text

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -1,5 +1,39 @@
+import action_bg from "../assets/GetInvolved.png";
+
 const Action = () => {
-  return <div>Action</div>;
+  return (
+    <div className="relative w-full h-[60vh] md:h-[40vh] flex justify-center">
+      <img
+        src={action_bg}
+        alt=""
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="w-[90%] mx-auto py-10 relative z-10">
+        <div className="flex flex-col items-center gap-5">
+          <div className="flex flex-row items-center gap-5">
+            <hr className="w-12 border-slate-300" />
+            <h1 className="font-sans text-md text-green-900 font-medium tracking-[0.2em] uppercase">
+              Join Us!
+            </h1>
+            <hr className="w-12 border-slate-300" />
+          </div>
+          <div className="flex flex-col text-center gap-5">
+            <h2 className="text-4xl md:text-5xl font-medium text-gray-600 leading-10 md:leading-12 tracking-tight capitalize">
+              Become a part of this life-changing journey.
+            </h2>
+            <p className="text-gray-600 font-sans px-5 md:text-lg">
+              Join us as we embark on the road to reclaiming our land.
+            </p>
+            <div className="flex justify-center">
+              <button className="bg-[#232d25] text-[#dddddd] font-medium py-3 px-5 md:py-5 md:px-7 md:text-lg font-sans capitalize rounded-xl">
+                Find out more
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Action;
