abf12de32f234e5ebf2a5966f82143967c985904
Rorisang Putu <rorisangputu@gmail.com>
2025-05-13 09:37:36 +0200

    feat(main): wrap App component with AppContextProvider for state management

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { AppContextProvider } from "./contexts/appContext.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,7 +18,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
