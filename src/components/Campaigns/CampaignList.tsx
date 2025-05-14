9ecad22eb028693c9f55d4adc10acbcfcf60e17d
Rorisang Putu <rorisangputu@gmail.com>
2025-05-14 13:31:41 +0200

    feat(CampaignsList): replace static campaign data with API fetch and handle loading state

--- src/components/Campaigns/CampaignList.tsx
+++ src/components/Campaigns/CampaignList.tsx
@@ -1,8 +1,27 @@
 import dotted from "../../assets/dotted_sa_bg.png";
 import CampaignTemp from "../CampaignTemp";
-import { campaigns } from "../../data/campaignData";
-
+import { useAppContext } from "../../contexts/appContext";
+import { useQuery } from "@tanstack/react-query";
+import * as apiClient from "../../apiClient";
 const CampaignsList = () => {
+  const { showToast } = useAppContext();
+  const { data: campaignData } = useQuery({
+    queryKey: ["getCampaigns"],
+    queryFn: async () => {
+      try {
+        return await apiClient.fetchCampaign();
+      } catch (error) {
+        showToast({ message: "Couldn't get Campaigns", type: "ERROR" });
+        throw error;
+      }
+    },
+  });
+
+  if (!campaignData) {
+    return <span>No Campaigns found.</span>;
+  }
+
+  //console.log(campaignData.data);
   return (
     <div className="w-full bg-green-900 py-10 relative overflow-hidden font-sans">
       {/* Background Image Positioned Behind Content */}
@@ -20,15 +39,15 @@ const CampaignsList = () => {
             <hr className="w-20" />
           </div>
           <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {campaigns.map((campaign) => (
+            {campaignData.map((campaign) => (
               <CampaignTemp
-                key={campaign.id}
-                id={campaign.id}
+                key={campaign._id}
+                id={campaign._id}
                 title={campaign.title}
                 location={campaign.location}
                 date={campaign.date}
                 description={campaign.description}
-                picture={campaign.picture}
+                picture={campaign.image}
               />
             ))}
           </div>
