e4b4fc193c9ac536bfa0e920234db52b18a5ad4f
Rorisang Putu <rorisangputu@gmail.com>
2025-05-14 13:56:35 +0200

    style(AdminDashboard): enhance layout with improved structure and styling

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,8 +1,14 @@
 const AdminDashboard = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-      <p>Welcome to the admin dashboard.</p>
+    <div className="w-full py-40">
+      <div className="w-[90%] mx-auto">
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+            <p>Welcome to the admin dashboard.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
