90d9c261752bbebb9762f957f750ce9c5a840445
Rorisang Putu <rorisangputu@gmail.com>
2025-05-19 09:38:28 +0200

    fix(NewCampaign): navigate to dashboard on successful campaign creation

--- src/pages/NewCampaign.tsx
+++ src/pages/NewCampaign.tsx
@@ -2,13 +2,16 @@ import { useMutation } from "@tanstack/react-query";
 import ManageCampaignForm from "../components/Admin/Create/ManageCampaignForm";
 import * as apiClient from "../apiClient";
 import { useAppContext } from "../contexts/appContext";
+import { useNavigate } from "react-router-dom";
 
 const NewCampaign = () => {
   const { showToast } = useAppContext();
+  const navigate = useNavigate();
   const { mutate, isPending } = useMutation({
     mutationFn: apiClient.createCampaign,
     onSuccess: async () => {
       showToast({ message: "Created Campaign", type: "SUCCESS" });
+      navigate("/admin/dashboard");
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: "ERROR" });
