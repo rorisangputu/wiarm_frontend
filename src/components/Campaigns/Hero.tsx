a74d794297e05c00f8a9a376f7d895273d62bf01
Rorisang Putu <rorisangputu@gmail.com>
2025-05-19 21:14:00 +0200

    fix(Hero): adjust height for better layout and improve image import consistency

--- src/components/Campaigns/Hero.tsx
+++ src/components/Campaigns/Hero.tsx
@@ -1,16 +1,16 @@
-import african_woman from "../../assets/GetInvolved.png";
+import bg from "../../assets/GetInvolved.png";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[60vh] md:h-[40vh] py-10 relative flex bg-gray-900 text-white">
+    <div className="w-full h-[40vh] xl:h-[40vh] py-10 relative flex bg-gray-900 text-white">
       <img
-        src={african_woman}
+        src={bg}
         alt=""
-        className="absolute inset-0 w-full h-full object-cover opacity-50"
+        className="absolute inset-0 w-full h-full  object-cover opacity-50"
       />
 
       <div className="w-[90%] mx-auto relative z-10">
-        <div className="flex flex-col gap-2 pt-24">
+        <div className="flex flex-col gap-2 pt-24 justify-center items-center">
           <div className="flex flex-row justify-center items-center gap-4">
             <hr className="w-12 border-slate-300" />
             <p className="tracking-[0.23em] uppercase text-md font-medium text-green-200">
@@ -18,7 +18,7 @@ const Hero = () => {
             </p>
             <hr className="w-12 border-slate-300" />
           </div>
-          <h1 className="capitalize font-sans text-4xl md:text-5xl font-semibold text-center">
+          <h1 className="capitalize font-sans text-4xl md:text-5xl xl:max-w-4xl mx-auto font-semibold text-center">
             Some Of Our Recent Campaigns That Helped Touch Lives
           </h1>
         </div>
