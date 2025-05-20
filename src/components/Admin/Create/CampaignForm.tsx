7240b64da652db8b2b08d546f146aa9e93ca9d57
Rorisang Putu <rorisangputu@gmail.com>
2025-05-20 08:56:12 +0200

    fix(CampaignForm): implement image upload and delete functionality with validation

--- src/components/Admin/Create/CampaignForm.tsx
+++ src/components/Admin/Create/CampaignForm.tsx
@@ -6,7 +6,22 @@ const CampaignForm = () => {
   const {
     register,
     formState: { errors },
+    watch,
+    setValue,
   } = useFormContext<CampaignFormData>();
+
+  const existingImageUrls = watch("images");
+
+  const handleDelete = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    imageUrl: string
+  ) => {
+    event.preventDefault();
+    setValue(
+      "images",
+      existingImageUrls.filter((url) => url !== imageUrl)
+    );
+  };
 
   return (
     <div className="max-w-xl mx-auto bg-white p-6 rounded shadow-md space-y-4">
@@ -63,6 +78,51 @@ const CampaignForm = () => {
           <span className="text-red-700">{errors.location.message}</span>
         )}
       </label>
+      <div>
+        <h2 className="text-2xl font-bold mb-3">Images</h2>
+        <div className="border rounded p-4 flex flex-col gap-4">
+          {existingImageUrls && (
+            <div className="grid grid-cols-6 gap-4">
+              {existingImageUrls.map((url) => (
+                <div className="relative group" key={url}>
+                  <img src={url} className="min-h-full object-cover" alt="" />
+                  <button
+                    onClick={(event) => handleDelete(event, url)}
+                    className="absolute inset-0 flex items-center justify-center
+                    bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 text-white"
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+          <input
+            type="file"
+            multiple
+            accept="image/*"
+            className="w-full text-gray-700 font-normal"
+            {...register("imageFiles", {
+              validate: (imageFiles) => {
+                //for editing campaign
+                const totalLength =
+                  imageFiles.length + (existingImageUrls?.length || 0);
+                if (totalLength === 0) {
+                  return "At least one image should be added";
+                }
+                if (totalLength > 6) {
+                  return "Images cannot be more than six (6)";
+                }
+              },
+            })}
+          />
+        </div>
+        {errors.imageFiles && (
+          <span className="text-red-600 text-sm font-bold">
+            {errors.imageFiles.message}
+          </span>
+        )}
+      </div>
     </div>
   );
 };
