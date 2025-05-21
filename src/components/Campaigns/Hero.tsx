578d931fa530c83894616fca706c59e503e1cf2a
Rorisang Putu <rorisangputu@gmail.com>
2025-05-21 09:58:27 +0200

    fix: adjust Hero component height and padding for improved layout

--- src/components/Campaigns/Hero.tsx
+++ src/components/Campaigns/Hero.tsx
@@ -2,7 +2,7 @@ import bg from "../../assets/GetInvolved.png";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[40vh] xl:h-[40vh] py-10 relative flex bg-gray-900 text-white">
+    <div className="w-full h-[50vh] xl:h-[50vh] py-10 relative flex bg-gray-900 text-white">
       <img
         src={bg}
         alt=""
@@ -10,7 +10,7 @@ const Hero = () => {
       />
 
       <div className="w-[90%] mx-auto relative z-10">
-        <div className="flex flex-col gap-2 pt-24 justify-center items-center">
+        <div className="flex flex-col gap-2 pt-20 justify-center items-center">
           <div className="flex flex-row justify-center items-center gap-4">
             <hr className="w-12 border-slate-300" />
             <p className="tracking-[0.23em] uppercase text-md font-medium text-green-200">
