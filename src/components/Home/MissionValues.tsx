a5e9540c56c668404611359b1f202640f133bd85
Rorisang Putu <rorisangputu@gmail.com>
2025-05-21 10:33:38 +0200

    fix: adjust padding and text size in MissionValues component for improved layout

--- src/components/Home/MissionValues.tsx
+++ src/components/Home/MissionValues.tsx
@@ -2,7 +2,7 @@ import wheatbg from "../../assets/wheatbg.png";
 
 const MissionValues = () => {
   return (
-    <div className="w-full bg-green-900 py-20 md:py-10 relative overflow-hidden">
+    <div className="w-full bg-green-900 py-20 md:py-24 relative overflow-hidden">
       {/* Background Image Positioned Behind Content */}
       <img
         src={wheatbg}
@@ -15,13 +15,13 @@ const MissionValues = () => {
         <div className="flex flex-col text-gray-100">
           <div className="flex items-center gap-4 justify-center mb-5">
             <hr className="w-12 border-t-2 border-[#cecece]" />
-            <h1 className="uppercase font-medium tracking-[0.2em] md:text-2xl md:py-10">
+            <h1 className="uppercase font-medium tracking-[0.2em] md:text-xl md:py-10">
               Mission & Values
             </h1>
             <hr className="w-12 border-t-2 border-[#cecece]" />
           </div>
 
-          <div className="flex flex-col gap-10 md:pb-10 md:flex-row md:text-lg">
+          <div className="flex flex-col gap-10 md:py-10 md:flex-row md:text-lg mx-auto">
             <div className="flex flex-col gap-5">
               <h1 className="text-gray-100 text-2xl font-medium">
                 Our Mission
